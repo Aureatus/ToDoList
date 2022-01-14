5841426e606a649467810b3f0392bed069e6354a
Alex Knott <alextknott@gmail.com>
2022-01-14 11:56:25 +0000

    Changed it to source mode so that it shows what file errors are from

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
