922532ce068a2992f271837f0736460bfe8cf981
Alex Knott <alextknott@gmail.com>
2022-01-14 15:17:34 +0000

    Added a default project that is created on startup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@
 
 // Use IIFE to createdefault "project" to put todos in, but let users create new projects and choose what projects the todos go in.
 
-import { todo } from "./appLogic";
+import { project } from "./appLogic";
 
-let todo1 = todo("a", "b", "c", "d");
-console.log(todo1);
-console.log(todo1.getTitle());
-console.log(todo1.getDescription());
-console.log(todo1.getDueDate());
-console.log(todo1.getPriority());
+let DefaultProject = project(
+  "Default",
+  "Default project created for all users."
+);
