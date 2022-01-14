b4c13e61a2aa815b4ba342ffbc988977582f9e5d
Alex Knott <alextknott@gmail.com>
2022-01-14 16:19:08 +0000

    Added style and css loader modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
   mode: "development",
   entry: {
@@ -18,5 +17,13 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
