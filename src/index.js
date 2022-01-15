3b528f88693cfe61e7813a810a8c692746758065
Alex Knott <alextknott@gmail.com>
2022-01-15 13:04:27 +0000

    Imported domManip module and ran it's initial function

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@
 // Use IIFE to createdefault "project" to put todos in, but let users create new projects and choose what projects the todos go in.
 
 import { project } from "./appLogic";
+import { domManip } from "./appDomManip";
+import "./style.css";
 
 let DefaultProject = project(
   "Default",
   "Default project created for all users."
 );
+
+domManip().initial();
