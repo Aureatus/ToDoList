a82180971d7e9f3b6fb8bd95a8934564ef0cd101
Alex Knott <alextknott@gmail.com>
2022-01-15 13:03:41 +0000

    Added domManip module with an initial function that is ran on startup

--- src/appDomManip.js
+++ src/appDomManip.js
@@ -1,2 +1,16 @@
-// Intial startup should create minion base
 import "./style.css";
+
+// Create a module containing all needed DOM manipulators.
+
+const domManip = () => {
+  const initial = () => {
+    let body = document.body;
+    let container = document.createElement("container");
+    body.appendChild(container);
+  };
+  return {
+    initial,
+  };
+};
+
+export { domManip };
