61bb70cc5b660588a1196e46f16463c4b6d57c0b
Alex Knott <alextknott@gmail.com>
2022-02-03 16:53:39 +0000

    Created functions for saving project and toDo data, formatting it into arrays of a) project names, b) project descriptions, and c) toDos, each index containing a toDoList.

---
+++ src/saveLogic.js
@@ -0,0 +1,49 @@
+import { projectsManager } from "./appLogic";
+
+const saveProjectListData = () => {
+  let projectNames = [];
+  let projectDescriptions = [];
+  projectsManager.getProjects().forEach((element, index, array) => {
+    projectNames.push(array[index].getName());
+    projectDescriptions.push(array[index].getDescription());
+  });
+
+  return [projectNames, projectDescriptions];
+};
+
+const saveToDoListData = () => {
+  let toDos = [];
+  projectsManager.getProjects().forEach((element, index, array) => {
+    let toDoList = [];
+    let toDoTitles = [];
+    let toDoDescriptions = [];
+    let toDoDueDates = [];
+    let toDoFormattedDueDates = [];
+    let toDoPriorities = [];
+    array[index].ToDoList.forEach(
+      (element,
+      (index) => {
+        toDoTitles.push(index.getTitle());
+        toDoDescriptions.push(index.getDescription());
+        toDoDueDates.push(index.getDueDate());
+        toDoFormattedDueDates.push(index.getFormattedDueDate());
+        toDoPriorities.push(index.getPriority());
+      })
+    );
+    for (let i = 0; i < toDoTitles.length; i++) {
+      let tempToDo = [];
+      tempToDo.push(
+        toDoTitles[i],
+        toDoDescriptions[i],
+        toDoDueDates[i],
+        toDoFormattedDueDates[i],
+        toDoPriorities[i]
+      );
+      toDoList.splice(i, 0, tempToDo);
+    }
+    toDos.push(toDoList);
+  });
+  return toDos;
+};
+
+export { saveProjectListData, saveToDoListData };
