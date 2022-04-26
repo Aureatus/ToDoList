06e869ab21e8777ffc7a17cb6b97f3f40025137c
Alex Knott <alextknott@gmail.com>
2022-04-26 16:12:20 +0100

    Implement rendering from firestore project and toDo data

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "normalize.css";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,63 +23,71 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const storageData = {};
-(function () {
-  let projectNames = JSON.parse(localStorage.getItem("projectNames"));
-  let projectDescriptions = JSON.parse(
-    localStorage.getItem("projectDescriptions")
+const getFirestoreData = async () => {
+  const database = getFirestore(app);
+  const projects = await getDocs(collection(database, "Projects"));
+  const toDoList = await getDocs(
+    collection(database, "Projects", "Project 1", "ToDoList")
   );
-  let toDos = JSON.parse(localStorage.getItem("toDos"));
-  if (projectNames) {
-    storageData.projectNames = projectNames;
-  }
-  if (projectDescriptions) {
-    storageData.projectDescriptions = projectDescriptions;
-  }
-  if (toDos) {
-    storageData.toDos = toDos;
-  }
-})();
-if (!storageData.projectNames && !storageData.projectDescriptions) {
-  let defaultProject = projectConstructor(
-    "Default",
-    "Default project created for all users."
-  );
-  projectsManager.pushProject(defaultProject);
-  projectsManager.changeSelectedProject(0);
-} else if (
-  storageData.projectNames ||
-  storageData.projectDescriptions ||
-  storageData.toDos
-) {
-  for (let i = 0; i < storageData.projectNames.length; i++) {
-    let tempProject = projectConstructor(
-      storageData.projectNames[i],
-      storageData.projectDescriptions[i]
-    );
-    projectsManager.pushProject(tempProject);
-  }
-  projectsManager.changeSelectedProject(0);
-}
 
-if (storageData.toDos) {
-  for (let i = 0; i < storageData.toDos.length; i++) {
-    storageData.toDos[i].forEach((element, index, array) => {
-      projectsManager
-        .getProjects()
-        [i].addToDo(element[0], element[1], parseISO(element[2]), element[4]);
-    });
-  }
-}
+  return { projects, toDoList };
+};
 
-domManip().initialBuild(projectsManager.getProjects());
-generalLogic().addEventListeners().projectAddButtonEventListener();
-generalLogic().addEventListeners().projectSubmitButtonEventListener();
-let currentProject = projectsManager.getSelectedProject();
-domManip().toDoRender(currentProject);
-generalLogic().addEventListeners().addToDoButtonEventListener();
-generalLogic().addEventListeners().addToDoSubmitButtonEventListener();
-generalLogic().addEventListeners().editToDoButtonEventListener();
-generalLogic().addEventListeners().addToDoDialogEventListener();
-generalLogic().addEventListeners().signInWithGoogleEventListener();
+// Initialize toDoList
+
+const initializeToDoList = async () => {
+  try {
+    const { projects, toDoList } = await getFirestoreData();
+    if (!projects) {
+      let defaultProject = projectConstructor(
+        "Default",
+        "Default project created for all users."
+      );
+      projectsManager.pushProject(defaultProject);
+      projectsManager.changeSelectedProject(0);
+    }
+    if (projects) {
+      projects.forEach((doc) => {
+        let tempProject = projectConstructor(
+          doc.data().name,
+          doc.data().description
+        );
+        projectsManager.pushProject(tempProject);
+      });
+
+      projectsManager.changeSelectedProject(0);
+    }
+    if (toDoList) {
+      const projects = projectsManager.getProjects();
+      projects.forEach((project, index) => {
+        toDoList.docs.forEach((doc) => {
+          projectsManager
+            .getProjects()
+            [index].addToDo(
+              doc.data().title,
+              doc.data().description,
+              new Date(doc.data().dueDate.seconds * 1000),
+              doc.data().priority
+            );
+        });
+      });
+    }
+
+    domManip().initialBuild(projectsManager.getProjects());
+    generalLogic().addEventListeners().projectAddButtonEventListener();
+    generalLogic().addEventListeners().projectSubmitButtonEventListener();
+    let currentProject = projectsManager.getSelectedProject();
+    domManip().toDoRender(currentProject);
+    generalLogic().addEventListeners().addToDoButtonEventListener();
+    generalLogic().addEventListeners().addToDoSubmitButtonEventListener();
+    generalLogic().addEventListeners().editToDoButtonEventListener();
+    generalLogic().addEventListeners().addToDoDialogEventListener();
+    generalLogic().addEventListeners().signInWithGoogleEventListener();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+initializeToDoList();
+
 export { projectsManager };
